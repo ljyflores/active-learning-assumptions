91f112b3f634b0b6a47d2c2705f8ca3ab97bd7d9
ljyflores <ljyflores@gmail.com>
2025-12-19 09:55:20 -0500

    Update results on 500 samples and plot

--- scripts/run_ablations_difficulty.sh
+++ scripts/run_ablations_difficulty.sh
@@ -5,7 +5,7 @@ declare -a seeds=("16" "27" "38")
 for dataset in "${datasets[@]}"; do
   for model in "${models[@]}"; do
     for seed in "${seeds[@]}"; do
-        sbatch --job-name=$dataset-$model-$seed --output=/home/mila/f/floresl/active-learning-assumptions/logs/$dataset-$model/output-$seed --error=/home/mila/f/floresl/active-learning-assumptions/logs/$dataset-$model/error-$seed ./scripts/run_ablations_mila_cluster.sh --dataset data/$dataset --model_name $model --epochs 200 --seed $seed --finetune_size 500 --wandb_off
+        sbatch --job-name=$dataset-$model-$seed --output=/home/mila/f/floresl/active-learning-assumptions/logs/$dataset-$model/output-$seed --error=/home/mila/f/floresl/active-learning-assumptions/logs/$dataset-$model/error-$seed ./scripts/run_ablations_mila_cluster.sh --dataset data/$dataset --model_name $model --epochs 200 --seed $seed --finetune_size 2000 --wandb_off
     done
   done
 done
